d6a614d3805b1b5be0489b42bd105e90ecbdfb09
bonsthie <barnabe.bonnet@gmail.com>
2024-11-25 12:55:53 +0100

    change msg to __VA_ARGS__ in log dummy

--- lua/fastclass/assets/log.lua
+++ lua/fastclass/assets/log.lua
@@ -4,7 +4,7 @@ return [[
 
 # if defined(VERBOSE) || defined(LOG)
 #  include <iostream>
-#  define LOG(...) std::cout << msg << std::endl;
+#  define LOG(...) std::cout << __VA_ARGS__ << std::endl;
 # else
 #  define LOG(...)
 # endif 
